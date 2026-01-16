d5fe1ec7109e3d7ac963431d319096a0f7e5b997
Alan Nemirovski <alannemirovski@gmail.com>
2026-01-16 11:16:30 -0500

    move code to folder

---
+++ code/Data Cleaning.R
@@ -0,0 +1,7 @@
+
+# ------------------------------------------------------------
+# Nettoyage des données: Analyse de contenu (Candidats 2025), Élections Montréal
+# Code écrit par Alan Nemirovski
+# ------------------------------------------------------------
+
+
